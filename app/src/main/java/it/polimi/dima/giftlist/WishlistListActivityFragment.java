c59c81134a8be759c7b27e166992dae792285a91
Alessandro Pappalardo <mail@alessandropappalardo.eu>
2015-12-18 13:05:22 +0100

    [Misc] First commit
    Signed-off-by: Alessandro Pappalardo <mail@alessandropappalardo.eu>

---
+++ app/src/main/java/it/polimi/dima/giftlist/WishlistListActivityFragment.java
@@ -0,0 +1,22 @@
+package it.polimi.dima.giftlist;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class WishlistListActivityFragment extends Fragment {
+
+    public WishlistListActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_wishlist_list, container, false);
+    }
+}
