7349cf876d26743250f593cd9260a48afbc7f3f1
Alessandro Pappalardo <mail@alessandropappalardo.eu>
2016-01-08 15:37:28 +0100

    [Model] Add item and wishlist model classes
    Signed-off-by: Alessandro Pappalardo <mail@alessandropappalardo.eu>

--- app/src/main/java/it/polimi/dima/giftlist/model/Item.java
+++ app/src/main/java/it/polimi/dima/giftlist/model/Item.java
@@ -4,4 +4,14 @@ package it.polimi.dima.giftlist.model;
  * Created by Alessandro on 08/01/16.
  */
 public class Item {
+
+    private String mName;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
 }
--- app/src/main/java/it/polimi/dima/giftlist/model/Wishlist.java
+++ app/src/main/java/it/polimi/dima/giftlist/model/Wishlist.java
@@ -1,7 +1,19 @@
 package it.polimi.dima.giftlist.model;
 
+import java.util.List;
+
 /**
  * Created by Alessandro on 08/01/16.
  */
 public class Wishlist {
+
+    private List<Item> mItemList;
+
+    public List<Item> getItemList() {
+        return mItemList;
+    }
+
+    public void setItemList(List<Item> itemList) {
+        mItemList = itemList;
+    }
 }
