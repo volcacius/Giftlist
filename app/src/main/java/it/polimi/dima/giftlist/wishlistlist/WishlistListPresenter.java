9f1422dff0efaa8b6f6859cac76a0813bc535bc5
Alessandro Pappalardo <mail@alessandropappalardo.eu>
2016-01-15 10:08:43 +0100

    [WishlistList] View and Presenter first commit

--- app/src/main/java/it/polimi/dima/giftlist/wishlistlist/WishlistListPresenter.java
+++ app/src/main/java/it/polimi/dima/giftlist/wishlistlist/WishlistListPresenter.java
@@ -1,7 +1,53 @@
 package it.polimi.dima.giftlist.wishlistlist;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import de.greenrobot.event.EventBus;
+import it.polimi.dima.giftlist.base.BaseRxLcePresenter;
+import it.polimi.dima.giftlist.model.Person;
+import it.polimi.dima.giftlist.model.Wishlist;
+import it.polimi.dima.giftlist.model.WishlistAddedEvent;
+import it.polimi.dima.giftlist.model.WishlistsProvider;
+import it.polimi.dima.giftlist.model.WishlistRemovedEvent;
+
 /**
  * Created by Alessandro on 08/01/16.
  */
-public class WishlistListPresenter {
+public class WishlistListPresenter extends BaseRxLcePresenter<WishlistListView, List<Wishlist>> {
+
+    protected EventBus eventBus;
+    protected WishlistsProvider wishlistsProvider;
+
+    @Inject
+    public WishlistListPresenter(WishlistsProvider wishlistsProvider, EventBus eventBus) {
+        this.eventBus = eventBus;
+        this.wishlistsProvider = wishlistsProvider;
+    }
+
+    @Override
+    public void attachView(WishlistListView view) {
+        super.attachView(view);
+        eventBus.register(this);
+    }
+
+    @Override
+    public void detachView(boolean retainInstance) {
+        super.detachView(retainInstance);
+        eventBus.unregister(this);
+    }
+
+    public void load(boolean pullToRefresh, Person person) {
+        subscribe(wishlistsProvider.getWishlistsOfPerson(person.getName()), pullToRefresh);
+    }
+
+    public void onEventMainThread(WishlistAddedEvent event) {
+        //TODO
+    }
+
+    public void onEventMainThread(WishlistRemovedEvent event) {
+        //TODO
+    }
+
 }
--- app/src/main/java/it/polimi/dima/giftlist/wishlistlist/WishlistListView.java
+++ app/src/main/java/it/polimi/dima/giftlist/wishlistlist/WishlistListView.java
@@ -1,7 +1,22 @@
 package it.polimi.dima.giftlist.wishlistlist;
 
+import com.hannesdorfmann.mosby.mvp.lce.MvpLceView;
+
+import java.util.List;
+
+import it.polimi.dima.giftlist.model.Wishlist;
+
 /**
  * Created by Alessandro on 08/01/16.
  */
-public interface WishlistListView {
+public interface WishlistListView extends MvpLceView<List<Wishlist>> {
+
+    public void addWishlist(int wishlistId);
+
+    public void removeWishlist(int wishlistId);
+
+    public void showAddingFailed(Wishlist wishlist);
+
+    public void showRemovingFailed(Wishlist wishlist);
+
 }
