744d5417432e84230bdcd3ef6cf883e29b1c4a1c
Elena Sacchi <elena.sacchi@mail.polimi.it>
2016-02-10 15:02:51 +0100

    [Util] Error handling. Now no internet connection is notified

--- app/src/main/java/it/polimi/dima/giftlist/util/ErrorMessageDeterminer.java
+++ app/src/main/java/it/polimi/dima/giftlist/util/ErrorMessageDeterminer.java
@@ -6,6 +6,15 @@ package it.polimi.dima.giftlist.util;
 public class ErrorMessageDeterminer {
 
     public String getErrorMessage(Throwable e, boolean pullToRefresh) {
-          return "An unknown error has occurred";
+        String message = e.getMessage();
+        switch (message) {
+            case HttpErrors.TIMEOUT:
+                return "You may have connection issues";
+            case HttpErrors.SERVER_ERROR:
+                return "Sorry, there are problems on the server side";
+            default:
+                System.out.print(message);
+                return "An unknown error has occurred";
+        }
     }
 }
---
+++ app/src/main/java/it/polimi/dima/giftlist/util/HttpErrors.java
@@ -0,0 +1,9 @@
+package it.polimi.dima.giftlist.util;
+
+/**
+ * Created by Elena on 09/02/2016.
+ */
+public class HttpErrors {
+     public final static String SERVER_ERROR = "HTTP 503 Service Unavailable";
+     public final static String TIMEOUT = "timeout";
+}
