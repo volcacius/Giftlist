d8551f90ac6086ec55df7baacc1b4a01e23ef463
Alessandro Pappalardo <mail@alessandropappalardo.eu>
2016-03-19 15:05:37 +0100

    [Dagger] Add PerActivity scope definition

---
+++ app/src/main/java/it/polimi/dima/giftlist/scope/PerActivity.java
@@ -0,0 +1,20 @@
+package it.polimi.dima.giftlist.scope;
+
+/**
+ * Created by Alessandro on 18/03/16.
+ */
+
+import java.lang.annotation.Retention;
+
+import javax.inject.Scope;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * A scoping annotation to permit objects whose lifetime should
+ * conform to the life of the activity to be memorized in the
+ * correct component.
+ */
+@Scope
+@Retention(RUNTIME)
+public @interface PerActivity {}
