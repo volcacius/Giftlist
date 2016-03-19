21fab4c789b81d33ac7530f821b18ad7332c7cde
Alessandro Pappalardo <mail@alessandropappalardo.eu>
2016-03-19 15:11:36 +0100

    [View] Refactor ProductList and  add Wishlist views

--- app/src/main/java/it/polimi/dima/giftlist/presentation/view/ProductListView.java
+++ app/src/main/java/it/polimi/dima/giftlist/presentation/view/ProductListView.java
@@ -5,10 +5,11 @@ import com.hannesdorfmann.mosby.mvp.lce.MvpLceView;
 import java.util.List;
 
 import it.polimi.dima.giftlist.data.model.EtsyProduct;
+import it.polimi.dima.giftlist.data.model.Product;
 
 /**
  * Created by Elena on 27/01/2016.
  */
-public interface ProductView extends MvpLceView<List<EtsyProduct>> {
+public interface ProductListView extends MvpLceView<List<Product>> {
     //useful for methods to change it dynamically from other activities
 }
---
+++ app/src/main/java/it/polimi/dima/giftlist/presentation/view/WishlistView.java
@@ -0,0 +1,19 @@
+package it.polimi.dima.giftlist.presentation.view;
+
+import com.hannesdorfmann.mosby.mvp.lce.MvpLceView;
+
+import java.util.List;
+
+import it.polimi.dima.giftlist.data.model.Product;
+import it.polimi.dima.giftlist.data.model.Wishlist;
+
+/**
+ * Created by Alessandro on 18/03/16.
+ */
+public interface WishlistView extends MvpLceView<Wishlist> {
+
+    public void addProduct(Product product);
+
+    public void removeProduct(Product product);
+
+}
