6066214a0f4b4ce474d2df20d1ec90e033c17fb3
Alessandro Pappalardo <mail@alessandropappalardo.eu>
2016-03-21 17:37:02 +0100

    [Component] Move Application component and add injection to data sources

--- app/src/main/java/it/polimi/dima/giftlist/ApplicationComponent.java
+++ app/src/main/java/it/polimi/dima/giftlist/ApplicationComponent.java
@@ -1,20 +1,20 @@
-package it.polimi.dima.giftlist.presentation.component;
+package it.polimi.dima.giftlist;
 
 import android.content.Context;
 
 import javax.inject.Singleton;
 
 import dagger.Component;
-import it.polimi.dima.giftlist.data.repository.ProductRepository;
+import it.polimi.dima.giftlist.data.repository.datasource.CurrencyDataSource;
+import it.polimi.dima.giftlist.domain.repository.ProductRepository;
+import it.polimi.dima.giftlist.data.repository.datasource.EtsyProductDataSource;
 import it.polimi.dima.giftlist.domain.executor.PostExecutionThread;
 import it.polimi.dima.giftlist.domain.executor.ThreadExecutor;
+import it.polimi.dima.giftlist.presentation.component.ProductListComponent;
 import it.polimi.dima.giftlist.presentation.module.ApplicationModule;
 import it.polimi.dima.giftlist.presentation.module.ProductListModule;
 import it.polimi.dima.giftlist.presentation.view.activity.BaseActivity;
-import it.polimi.dima.giftlist.presentation.view.activity.BaseViewStateLceActivity;
-import it.polimi.dima.giftlist.presentation.view.activity.MainActivity;
 import it.polimi.dima.giftlist.presentation.view.fragment.BaseFragment;
-import it.polimi.dima.giftlist.presentation.view.fragment.BaseViewStateLceFragment;
 
 /**
  * Created by Alessandro on 15/03/16.
@@ -26,6 +26,8 @@ public interface ApplicationComponent {
 
     void inject(BaseActivity baseActivity);
     void inject(BaseFragment baseFragment);
+    void inject(EtsyProductDataSource etsyProductDataSource);
+    void inject(CurrencyDataSource currencyDataSource);
 
     //Naming convention for this method is: returned type is a subcomponent class, method name is arbitrary, parameters are modules required in this subcomponent.
     ProductListComponent plus(ProductListModule module);
