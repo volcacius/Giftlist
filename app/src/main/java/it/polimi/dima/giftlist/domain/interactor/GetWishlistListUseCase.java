54d1b73d8df75f0afcf1b36b3490d3e6982a267e
Alessandro Pappalardo <mail@alessandropappalardo.eu>
2016-03-23 21:44:04 +0100

    [UseCase] Update to RxAndroid execution parameters, remove references to custom executors

--- app/src/main/java/it/polimi/dima/giftlist/domain/interactor/GetWishlistListUseCase.java
+++ app/src/main/java/it/polimi/dima/giftlist/domain/interactor/GetWishlistListUseCase.java
@@ -5,19 +5,12 @@ import java.util.List;
 import javax.inject.Inject;
 
 import it.polimi.dima.giftlist.data.model.Wishlist;
-import it.polimi.dima.giftlist.domain.executor.PostExecutionThread;
-import it.polimi.dima.giftlist.domain.executor.ThreadExecutor;
 import rx.Observable;
 
 /**
  * Created by Alessandro on 18/03/16.
  */
 public class GetWishlistListUseCase extends UseCase<List<Wishlist>> {
-
-    @Inject
-    public GetWishlistListUseCase(ThreadExecutor threadExecutor, PostExecutionThread postExecutionThread) {
-        super(threadExecutor, postExecutionThread);
-    }
 
     @Override
     protected Observable<List<Wishlist>> buildUseCaseObservable() {
--- app/src/main/java/it/polimi/dima/giftlist/domain/interactor/UseCase.java
+++ app/src/main/java/it/polimi/dima/giftlist/domain/interactor/UseCase.java
@@ -1,10 +1,10 @@
 package it.polimi.dima.giftlist.domain.interactor;
 
-import it.polimi.dima.giftlist.domain.executor.PostExecutionThread;
-import it.polimi.dima.giftlist.domain.executor.ThreadExecutor;
 import rx.Observable;
+import rx.Scheduler;
 import rx.Subscriber;
 import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 import rx.subscriptions.Subscriptions;
 
@@ -13,22 +13,14 @@ import rx.subscriptions.Subscriptions;
  */
 public abstract class UseCase<T> {
 
-    private final ThreadExecutor threadExecutor;
-    private final PostExecutionThread postExecutionThread;
-
     private Subscription subscription = Subscriptions.empty();
-
-    public UseCase(ThreadExecutor threadExecutor, PostExecutionThread postExecutionThread) {
-        this.threadExecutor = threadExecutor;
-        this.postExecutionThread = postExecutionThread;
-    }
 
     protected abstract Observable<T> buildUseCaseObservable();
 
     public void execute(Subscriber<T> subscriber) {
         this.subscription = this.buildUseCaseObservable()
-                .subscribeOn(Schedulers.from(threadExecutor))
-                .observeOn(postExecutionThread.getScheduler())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(subscriber);
     }
 
