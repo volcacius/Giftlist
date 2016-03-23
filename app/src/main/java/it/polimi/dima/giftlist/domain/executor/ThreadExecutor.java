71d9032e5d9acaaceffbfc771f710bec0a56a4ff
Alessandro Pappalardo <mail@alessandropappalardo.eu>
2016-03-23 21:21:23 +0100

    [Executor] Remove custom executors classes and interfaces, now uses RxAndroid standard implementation

--- app/src/main/java/it/polimi/dima/giftlist/domain/executor/ThreadExecutor.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.dima.giftlist.domain.executor;
-
-import java.util.concurrent.Executor;
-
-/**
- * Created by Alessandro on 18/03/16.
- */
-public interface ThreadExecutor extends Executor {
-}
