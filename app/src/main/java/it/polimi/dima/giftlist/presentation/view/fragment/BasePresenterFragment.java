2ec145c67ad4e58a0121fc34163a53bd61fb3be3
Elena Sacchi <elena.sacchi@mail.polimi.it>
2016-08-11 14:27:14 +0200

    [Base] Added base fragment for simple fragments with presenter but no LCE

---
+++ app/src/main/java/it/polimi/dima/giftlist/presentation/view/fragment/BasePresenterFragment.java
@@ -0,0 +1,96 @@
+package it.polimi.dima.giftlist.presentation.view.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hannesdorfmann.fragmentargs.FragmentArgs;
+import com.hannesdorfmann.mosby.mvp.MvpFragment;
+import com.hannesdorfmann.mosby.mvp.MvpPresenter;
+import com.hannesdorfmann.mosby.mvp.MvpView;
+import com.squareup.leakcanary.RefWatcher;
+
+import org.greenrobot.eventbus.EventBus;
+
+import javax.inject.Inject;
+
+import butterknife.ButterKnife;
+import icepick.Icepick;
+import it.polimi.dima.giftlist.GiftlistApplication;
+import it.polimi.dima.giftlist.di.HasComponent;
+import it.polimi.dima.giftlist.presentation.navigation.IntentStarter;
+
+/**
+ * Created by Elena on 11/08/2016.
+ */
+public abstract class BasePresenterFragment <V extends MvpView, P extends MvpPresenter<V>> extends MvpFragment <V, P>{
+    @Inject
+    IntentStarter intentStarter;
+
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FragmentArgs.inject(this);
+    }
+
+    @LayoutRes protected abstract int getLayoutRes();
+
+    @Nullable @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        Icepick.restoreInstanceState(this, savedInstanceState);
+        return inflater.inflate(getLayoutRes(), container, false);
+    }
+
+    @Override public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Icepick.saveInstanceState(this, outState);
+    }
+
+    @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        injectDependencies();
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+    }
+
+    @Override public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        RefWatcher refWatcher = GiftlistApplication.getRefWatcher(getActivity());
+        refWatcher.watch(this);
+    }
+
+    /**
+     * Inject the dependencies. The component itself is declared in the Application class
+     * so its lifecycle is fine since it's tied to the Application
+     */
+    protected abstract void injectDependencies();
+
+    /**
+     * Gets a component for dependency injection by its type.
+     * This pattern allows to avoid ugly casting in each fragment
+     */
+    @SuppressWarnings("unchecked")
+    protected <C> C getComponent(Class<C> componentType) {
+        return componentType.cast(((HasComponent<C>) getActivity()).getComponent());
+    }
+
+    protected void replaceFragment(int containerViewId, Fragment fragment) {
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack so the user can navigate back
+        transaction.replace(containerViewId, fragment);
+        transaction.addToBackStack(null);
+        // Commit the transaction
+        transaction.commit();
+    }
+}
